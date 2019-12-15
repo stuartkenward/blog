f95f6edd27a60a4247be23ceea11e44c1a2031cf
Stuart Kenward <870278@swansea.ac.uk>
2019-12-15 22:25:45 +0000

    init

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="col-9">
+        <div>
+            <h1>User 1</h1>
+        </div>
+        <div>
+            <div><strong>153</strong> posts</div>
+            <div><strong>23k</strong> followers</div>
+            <div><strong>212</strong> following</div>
+        </div>
+    </div>
+
+    <div class="row pt-5">
+            <div class="col-12">
+                <h3>Blog post 1</h3>
+            </div>
+    </div>
+</div>
+@endsection
