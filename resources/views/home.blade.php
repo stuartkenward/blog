2bd1eb83ff992c2226a07a0cf15f8b79153948a1
Stuart Kenward <870278@swansea.ac.uk>
2019-12-17 18:24:46 +0000

    Adding temp layouts for testing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,69 @@
 
 @section('content')
 <div class="container">
-    <div class="col-9">
+    
+    <div class="col-9" style="
+    background: #FFFFFF; 
+    canvas: #DAE0E6; 
+    display: block; 
+    border-radius: 4px; 
+    cursor: pointer; 
+    box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.075) !important; 
+    transition: color .5s,fill .5s,box-shadow .5s;">
         <div>
-            <h1>User 1</h1>
-        </div>
-        <div>
-            <div><strong>153</strong> posts</div>
-            <div><strong>23k</strong> followers</div>
-            <div><strong>212</strong> following</div>
+            <h1>Title</h1>
+            <div>
+                <h4>Content goes here</h3>
+            </div>
+            <div>
+                <div>Username</div>
+                <div>Date Posted</div>
+            </div>
         </div>
     </div>
 
-    <div class="row pt-5">
-            <div class="col-12">
-                <h3>Blog post 1</h3>
+    <div style="padding-top: 16px"></div>
+
+    <div class="col-9" style="
+    background: #FFFFFF; 
+    canvas: #DAE0E6; 
+    display: block; 
+    border-radius: 4px; 
+    cursor: pointer; 
+    box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.075) !important; 
+    transition: color .5s,fill .5s,box-shadow .5s;">
+        <div>
+            <h1>Title</h1>
+            <div>
+                <h4>Content goes here</h3>
             </div>
+            <div>
+                <div>Username</div>
+                <div>Date Posted</div>
+            </div>
+        </div>
+    </div>
+
+    <div style="padding-top: 16px"></div>
+
+    <div class="col-9" style="
+    background: #FFFFFF; 
+    canvas: #DAE0E6; 
+    display: block; 
+    border-radius: 4px; 
+    cursor: pointer; 
+    box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.075) !important; 
+    transition: color .5s,fill .5s,box-shadow .5s;">
+        <div>
+            <h1>Title</h1>
+            <div>
+                <h4>Content goes here</h3>
+            </div>
+            <div>
+                <div>Username</div>
+                <div>Date Posted</div>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
