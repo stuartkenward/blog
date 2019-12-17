41e1f6eccbe497a76f3f12d2fcb547b4c6fd18c4
Stuart Kenward <870278@swansea.ac.uk>
2019-12-17 21:55:49 +0000

    Changing posts layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,36 +2,37 @@
 
 @section('content')
 
-<div class="py-4">
-<div class="container">
+<div >
     <div class="container" style="
         border-radius: 4px;
         box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.075) !important;
         background: #FFFFFF; 
         padding-top: 16px">
 
-    <div class="">
-        <div class="search-container">
-            <form action="/action_page.php">
-                <input type="text" placeholder="Search.." name="search">
-                <button type="submit"><i class="fa fa-search"></i></button>
-            </form>
-        </div>
+        <div class="">
+            <div class="search-container">
+                <form action="/action_page.php">
+                    <input type="text" placeholder="Search.." name="search">
+                    <button type="submit"><i class="fa fa-search"></i></button>
+                </form>
+            </div>
 
-        <select name="sortBy">
-            <option value="DateNew">Date new to old</option>
-            <option value="DateOld">Date old to new</option>
-            <option value="Popularity">Popularity</option>
-        </select>
-        <select name="sortByCategory">
-            <option value="Category1">Category 1</option>
-            <option value="Category2">Category 2</option>
-            <option value="Category3">Category 3</option>
-        </select>     
+            <select name="sortBy">
+                <option value="DateNew">Date new to old</option>
+                <option value="DateOld">Date old to new</option>
+                <option value="Popularity">Popularity</option>
+            </select>
+            <select name="sortByCategory">
+                <option value="Category1">Category 1</option>
+                <option value="Category2">Category 2</option>
+                <option value="Category3">Category 3</option>
+            </select>     
+        </div>
     </div>
 
+<div class="py-2">
 @foreach ($posts as $post)
-    <div class="col-12" style="
+    <div class="container" style="
     background: #FFFFFF; 
     canvas: #DAE0E6; 
     display: block; 
@@ -52,9 +53,8 @@
 @endforeach
 
 {{ $posts->links() }}
-
-    
 </div>
+    
 </div>
 </div>
 @endsection
