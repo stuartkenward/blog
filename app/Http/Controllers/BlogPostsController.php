0d5558d943b44a479d8be517d7a994353896f011
Stuart Kenward <870278@swansea.ac.uk>
2019-12-18 19:20:19 +0000

    Adding auth

--- app/Http/Controllers/BlogPostsController.php
+++ app/Http/Controllers/BlogPostsController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\BlogPosts;
 use App\Comments;
-use App\BlogPost;
-use App\User;
 
 class BlogPostsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index(){
 
         $posts = BlogPosts::paginate(10);
